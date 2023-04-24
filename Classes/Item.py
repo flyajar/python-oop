a410e191ec983c072c6227803755e39938e435c8
flyajar <rj.jovenal@gmail.com>
2023-04-24 16:03:59 +0800

    class methods

--- Classes/Item.py
+++ Classes/Item.py
@@ -1,4 +1,11 @@
+import csv
+
+
 class Item:
+    # class attribute
+    pay_rate = 0.8
+    all = []
+
     # constructor
     def __init__(self, name: str, price: float, quantity: int):
         # validate the arguments
@@ -6,14 +13,36 @@ class Item:
         assert quantity >= 0, f"Quantity should be a non-negative number"
 
         # assign to self object
+        # these are instance attribute
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        # actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item("Phone", 2, 5)
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-print(item1.calculate_total_price())
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('./../items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+
+Item.instantiate_from_csv()
+print(Item.all)
