f414fd5940ed55598cb8fa9bb8312b5d7cea639d
flyajar <rj.jovenal@gmail.com>
2023-04-24 13:40:30 +0800

    added the type hinting and validation of arguments

--- Classes/Item.py
+++ Classes/Item.py
@@ -1,6 +1,11 @@
 class Item:
     # constructor
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: float, quantity: int):
+        # validate the arguments
+        assert price >= 0, f"Price should be non-negative number"
+        assert quantity >= 0, f"Quantity should be a non-negative number"
+
+        # assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -9,6 +14,6 @@ class Item:
         return self.price * self.quantity
 
 
-item1 = Item('phone', 100, 5)
+item1 = Item("Phone", 2, 5)
 
 print(item1.calculate_total_price())
